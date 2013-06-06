4cfd98718b9d4cd2eb6039042e860787162eece0
Venj <ersaclarke@gmail.com>
2013-06-06 23:13:15 +0800

    Reverse to %252F solution...

--- Video Player/VPFileListViewController.m
+++ Video Player/VPFileListViewController.m
@@ -96,7 +96,7 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     NSString *fileName = [self.movieFiles[indexPath.row] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-    fileName = [fileName stringByReplacingOccurrencesOfString:@"/" withString:@"%2F"];
+    fileName = [fileName stringByReplacingOccurrencesOfString:@"/" withString:@"%252F"];
     __block VPFileListViewController *blockSelf = self;
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     NSString *path = [defaults objectForKey:ServerPathKey];

5b20f57c75f89ddfa385f67ed8959655d5d11d5b
Venj Chu <ersaclarke@gmail.com>
2013-12-11 10:45:50 +0800

    Updated InAppSettingsKit.

--- Podfile
+++ Podfile
@@ -3,7 +3,7 @@ platform :ios, '5.1'
 inhibit_all_warnings!
 
 pod 'AFNetworking', '~> 1.2'
-pod 'InAppSettingsKit', '~> 1.1'
+pod 'InAppSettingsKit', '~> 2.0'
 pod 'BlocksKit', '~> 2.0.0'
 pod 'MWPhotoBrowser', '~> 1.1.4'
 pod 'KKPasscodeLock', '~> 0.2.2'
--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -56,8 +56,8 @@
 
 // InAppSettingsKit
 #define COCOAPODS_POD_AVAILABLE_InAppSettingsKit
-#define COCOAPODS_VERSION_MAJOR_InAppSettingsKit 1
-#define COCOAPODS_VERSION_MINOR_InAppSettingsKit 1
+#define COCOAPODS_VERSION_MAJOR_InAppSettingsKit 2
+#define COCOAPODS_VERSION_MINOR_InAppSettingsKit 0
 #define COCOAPODS_VERSION_PATCH_InAppSettingsKit 0
 
 // KKPasscodeLock

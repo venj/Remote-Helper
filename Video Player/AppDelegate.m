0faaa6f380b546d435aecb3a0bf0f8383e26703e
Venj Chu <ersaclarke@gmail.com>
2014-12-31 13:20:13 +0800

    Revert to original solution.

--- Video Player/AppDelegate.m
+++ Video Player/AppDelegate.m
@@ -105,11 +105,11 @@
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    [self showPassLock];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    [self showPassLock];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
